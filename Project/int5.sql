99af734601b2c475d0133c5d961a34ba7671cd5d
Diogo Nunes <diogoffn@gmail.com>
2022-01-24 10:58:35 -0800

    query 5

--- Project/int5.sql
+++ Project/int5.sql
@@ -1,3 +1,5 @@
+--numero de episodios de cada serie
+
 SELECT Show.showname AS Show,
 	  COUNT(Episode.epnumber) AS Numero_de_Episodios 
 	  FROM Show, Season, Episode WHERE Season.seasonID = Episode.seasonID AND Show.showID = Season.showID GROUP BY Show.showname;
