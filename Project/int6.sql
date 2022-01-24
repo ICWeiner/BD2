77f171661555f3b48a9f550911d4aac5119f3d43
CarlosMealha <up202005954@edu.fc.up.pt>
2022-01-24 11:51:30 -0800

    int6.sql

---
+++ Project/int6.sql
@@ -0,0 +1,14 @@
+.bail ON
+.mode columns
+.headers on
+.nullvalue NULL
+PRAGMA foreign_keys = ON;
+
+-- Nome de perfis da conta do Rodrigo Monteiro que a sua data de nascimento foi entre 1950 e 2001.
+
+SELECT Profile.profilename AS name, Profile.birthdate AS Data
+FROM Profile, User
+WHERE Data BETWEEN '1950-01-01' AND '2001-12-31'
+AND User.userID = Profile.userID
+AND Profile.userID = (SELECT userID from User WHERE username = "Rodrigo Monteiro")
+GROUP BY Data;
